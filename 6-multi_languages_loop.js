17c0057b86e061a1627ce42506b0ebfa93eea5d8
Atokolo John <atokoloj@gmail.com>
2025-05-20 20:54:39 +0100

    added 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-let myArr = [ "C is fun", "Python is cool", "JavaScript is amazing"];
+const myArr = [ “C is fun", “Python is cool”, “JavaScript is amazing” ]  
 
-for(let i = 0; i < myArr.length; i++) {
-    console.log(myArr[i]);
+    for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
 }
