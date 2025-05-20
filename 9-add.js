a1cc7b482ee8c4f6598085a017881f312cbb7b79
Atokolo John <atokoloj@gmail.com>
2025-05-20 22:09:41 +0100

    added 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
-const a = args[0]
-const b = args[1]
+const a = parseInt(args[0],10)
+const b = parseInt(args[1],10)
 function add(a, b) {
   console.log(a + b);
 }
