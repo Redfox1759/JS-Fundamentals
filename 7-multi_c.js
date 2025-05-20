a6dd1343f522536fb2351d4c652ae60fc9835e64
Atokolo John <atokoloj@gmail.com>
2025-05-20 21:31:56 +0100

    added 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,6 +4,6 @@ const num = parseInt(args[0],10);
 if(isNaN(num) || !num){
     console.log("Misiing number")
 }else{
-    for(let i = 0; i < num; i++)
+    for (let i = 0; i < num; i++)
     console.log("C is fun")
 }
