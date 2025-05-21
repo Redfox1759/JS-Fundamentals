7b38411356108c0f571a29f6140659395a9dbbe3
Atokolo John <atokoloj@gmail.com>
2025-05-21 15:53:50 +0100

    added file

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,15 +1,13 @@
-const args = process.argv.slice(2);
-
-const num = parseInt(args[0], 10);  
-
-function factorial(n)
-{
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (1);
-	else
-		return (n * factorial(n - 1));
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+    } else{
+        n * factorial(n - 1);
+    }   
 }
 
-factorial(num);
+const firstArg = process.argv[2];
+
+const numberToFactorial = parseInt(firstArg, 10);
+
+console.log(factorial(numberToFactorial));
