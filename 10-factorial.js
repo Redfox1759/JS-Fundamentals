dffa9632330d787addab39733e5c4d4c8d31443b
Atokolo John <atokoloj@gmail.com>
2025-05-21 15:30:18 +0100

    added 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,15 @@
 const args = process.argv.slice(2);
 
 const num = parseInt(args[0], 10);  
-function factorial(n) {
-  if (n === 0 || !args[0]) {
-    console.log(1);
-  }
-    console.log(n * factorial(n - 1));
+
+function factorial(n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
+
 factorial(num);
